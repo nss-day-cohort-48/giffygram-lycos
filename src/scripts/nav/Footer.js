fa3bb84a1fe432616c09b22ea755f1136fc645ae
Tom Clouse <tommyclouse@yahoo.com>
2021-05-19 14:56:41 -0500

    first commit

---
+++ src/scripts/nav/Footer.js
@@ -0,0 +1,32 @@
+import {getUsers} from ######.js
+
+const 
+
+export const Footer = () => {
+
+    const users = getUsers()
+
+    const yearsDropdown = {
+    return`
+    <footer class="footer">
+
+    <div class="footer__item footer__years">
+    Post since <select id="yearSelection">
+    <option>2021</option>
+    <option>2020</option>
+    <option2019</option>
+    <option>2018</option>
+    <option>2017</option>
+    </select>
+    </div> 
+    `}
+
+
+    <div class="footer__item footer__postsByUser">
+    Posts by user <select id="userSelection">
+    
+
+
+
+    </footer>
+}
