97e453b16c2f8e5943b84b2753dae4aa32f8d0e9
Tom Clouse <tommyclouse@yahoo.com>
2021-05-19 17:43:59 -0500

    rough outline

--- src/scripts/nav/Footer.js
+++ src/scripts/nav/Footer.js
@@ -1,15 +1,7 @@
-import {getUsers} from ######.js
+import { getUsers, getLikes } from provider.js
 
-const 
-
-export const Footer = () => {
-
-    const users = getUsers()
-
-    const yearsDropdown = {
-    return`
-    <footer class="footer">
-
+const yearsDropdown = () => {
+    return `
     <div class="footer__item footer__years">
     Post since <select id="yearSelection">
     <option>2021</option>
@@ -22,11 +14,39 @@ export const Footer = () => {
     `}
 
 
-    <div class="footer__item footer__postsByUser">
-    Posts by user <select id="userSelection">
-    
+const UserDropDown = () => {
+    const users = getUsers()
+    let html = "<div class='footer__item footer__postsByUser'>"
+    html += "Posts by user <select id='userSelection'>"
+    html += users.map((user) => {
+        return `
+            <option id="user--${user.id}">${user.name}</option>
+            `
+    }
+    )
+}
 
+//need to figure out this is made specific to the User that is logged in.
+const filterFavorites = () => {
+    const likes = getLikes()
+    for (const like of likes) {
+        if (like = true) {
+            return like
+        }
+    }
+}
 
+export const Footer = () => {
 
-    </footer>
+    let html = "<footer class='footer'>"
+
+    html += yearsDropdown()
+
+    html += UserDropDown()
+
+    html += filterFavorites()
+
+    html = + "</footer>"
+
+    return html
 }
