39c9a0c9cd77880d2628027bfd4b00e980842305
Lunderwood33 <donnie.underwood.11@gmail.com>
2021-05-21 12:23:13 -0400

    updating code

--- src/scripts/message/MessageForm.js
+++ src/scripts/message/MessageForm.js
@@ -2,17 +2,25 @@
 
 
 export const MessageForm = () => {
-    return `
+    return ` <div class="class = directMessage">
         <h2>Direct Message</h2>
+        <h3>Recipient:</h3>
+        <div class="dropdown">
+  <button onclick="myFunction()" class="dropbtn">Choose a recipient</button>
+  <div id="myDropdown" class="dropdown-content">
+    <a href="#1">Link 1</a>
+    <a href="#2">Link 2</a>
+    <a href="#3">Link 3</a>
+  </div>
+</div>
         <fieldset>
-            <input id="recipientName" placeholder="Recipient:" type="text" />
+            <input id="chooseRecipient" placeholder="Choose a recipient" type="dropdown" />
         </fieldset>
+        <h3>Message:</h3>
         <fieldset>
-            <input id="userMessage" placeholder="Message:" type="text" />
+            <input id="userMessage" placeholder="Message to user" type="text" />
         </fieldset>
-        <input id="saveButton" placeholder="Save" type="button" />
-        </fieldset>
-        <input id="cancelButton" placeholder="Cancel" type="button" />
-        </fieldset>
+            <button onclick="myFunction()">Save</button>
+        <button onclick="myFunction()">Cancel</button></div>
     `
 }
